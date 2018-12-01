5d18a1e69fd9170fdf2fb9262cc682e4d52f37bd
Sebastian <sebastian.peredom@gmail.com>
2018-12-01 05:41:01 -0500

    second commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="img">
+      <img width="275px" height="83px" src="./assets/logo-urp.jpg">
+    </div>
+    <div id="container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "App"
+};
+</script>
+<style>
+@import url("https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css");
+@import "../node_modules/material-design-icons/iconfont/material-icons.css";
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#img {
+  text-align: center;
+}
+#container {
+  margin: auto;
+  width: 90%;
+  padding: 10px;
+}
+</style>
---
+++ src/components/donacion-dapp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    
+    <hello-metamask/>
+  </div>
+</template>
+
+<script>
+import HelloMetamask from '@/components/hello-metamask'
+import PublicacionComponent from '@/components/publicacion-component'
+export default {
+  name: "donacion-dapp",
+  beforeCreate() {
+    console.log("registerWeb3 Action dispatched from casino-dapp.vue");
+    this.$store.dispatch("registerWeb3");
+  },
+  components: {
+    'hello-metamask': HelloMetamask,
+    'publicacion-component': PublicacionComponent
+  }
+};
+</script>
+<style scoped>
+</style>
